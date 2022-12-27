3861d196657712e323fc3ad46f3e7e8e8ecea4ba
JanMontilivi03 <121024601+JanMontilivi03@users.noreply.github.com>
2022-12-27 10:26:24 +0100

    Update contador3.php

--- contador3.php
+++ contador3.php
@@ -1,16 +1,16 @@
-<? php
- 
-  $contador=0;
-if($fd = fopen('counter.txt','r')){
-      $contador=fgets($fd);
-}
-fclose($fd);
+<?php
 
-$fd2 = fopen(('counter.txt','w');
-$contador = $contador+1;
+$contador=0;
+
+if($fd = fopen('counter.txt','r')){
+   $contador=fgets($fd);
+   fclose($fd);
+}
+
+$fd2=fopen('counter.txt','w');
+$contador=$contador+1;
 fwrite($fd2,$contador);
 fclose($fd2);
-             
- echo $contador;
-?>
+
+echo $contador;
 
